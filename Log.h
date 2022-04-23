f9523d3b7666a3046b8c9911b0c5292a9486fad1
Team19 <team19@eecs.uci.edu>
2022-04-23 00:31:07 -0700

    commit message

---
+++ Log.h
@@ -0,0 +1,11 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include "Game.h"
+
+
+void PlayerLog(Game *game, int Rank1, int File1, int Rank2, int File2);
+
+void ComputerLog(Game *game, int Rank1, int File1, int Rank2, int File2);
+
+#endif
